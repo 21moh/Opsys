7368f6b1168e493d2a0411bd786063b8287ed800
21moh <68302300+21moh@users.noreply.github.com>
2023-06-24 17:07:50 -0400

    Update hw2.c

--- hw2.c
+++ hw2.c
@@ -17,19 +17,19 @@ bool checkQueenPlacement(char** grid, int row, int column, int m, int n) {
     }
 	for (int i = 0; i < m; i++) {
     	for (int j = 0; j < n; j++) {
-    		*(*(copyGrid + i) + j) = *(*(grid + i) + j)
+    		*(*(copyGrid + i) + j) = *(*(grid + i) + j);
     	}
     }
-    *(*(copyGrid + row) + column) = 'Q';
 
     // check for attackers vertically |
+
+
 
     for (int i = 0; i < m; i++) {
     	if (*(*(copyGrid + i) + column) == 'Q') {
     		return false;
     	}
     }
-
     // check for attackers horizontally ---
 
     for (int j = 0; j < n; j++) {
@@ -46,16 +46,17 @@ bool checkQueenPlacement(char** grid, int row, int column, int m, int n) {
     while(rowCheck != m && colCheck != n) {
     	rowCheck++;
     	colCheck++;
-    	if (rowCheck > m) {
+    	if (rowCheck >= m) {
     		break;
     	}
-    	if (colCheck > n) {
+    	if (colCheck >= n) {
     		break;
     	}
     	if (*(*(copyGrid + rowCheck) + colCheck) == 'Q') {
     		return false;
     	}
     }
+
 
     // check mid to top left
     rowCheck = row;
@@ -80,7 +81,7 @@ bool checkQueenPlacement(char** grid, int row, int column, int m, int n) {
     while(rowCheck != m && colCheck != 0) {
     	rowCheck++;
     	colCheck--;
-    	if (rowCheck > m) {
+    	if (rowCheck >= m) {
     		break;
     	}
     	if (colCheck < 0) {
@@ -90,7 +91,6 @@ bool checkQueenPlacement(char** grid, int row, int column, int m, int n) {
     		return false;
     	}
     }
-
     // check mid to top right
     rowCheck = row;
     colCheck = column;
@@ -100,7 +100,7 @@ bool checkQueenPlacement(char** grid, int row, int column, int m, int n) {
     	if (rowCheck < 0) {
     		break;
     	}
-    	if (colCheck > n) {
+    	if (colCheck >= n) {
     		break;
     	}
     	if (*(*(copyGrid + rowCheck) + colCheck) == 'Q') {
@@ -108,6 +108,39 @@ bool checkQueenPlacement(char** grid, int row, int column, int m, int n) {
     	}
     }
     return true;
+
+}
+
+void SolutionFinder(char** grid, int row, int m, int n, int solutions, int pipefd[2]){
+    if (row == m) {
+        solutions++; 
+        printf("P%d: Found a solution; notifying top-level parent\n", getpid());
+        // Notify the parent process through the pipe
+        close(pipefd[0]);  // Close the read end of the pipe
+        write(pipefd[1], solutions, sizeof(int));
+        close(pipefd[1]);  // Close the write end of the pipe
+        exit(EXIT_SUCCESS);
+    }
+    for (int i = 0; i < n; i++) {
+        if (checkQueenPlacement(grid, row, i, m, n)) {
+            printf("P%d: 1 possible move at row #%d; creating 1 child process...\n", getpid(), row);
+            pid_t child_pid = fork();
+            if (child_pid == -1) {
+                perror("fork");
+                exit(EXIT_FAILURE);
+            } else if (child_pid == 0) {
+                *(*(grid + row) + i) = 'Q';
+                SolutionFinder(grid, row + 1, m, n, solutions, pipefd);
+                exit(EXIT_SUCCESS);
+            }
+        }
+    }
+
+    // dead end spot
+    printf("P%d: Dead end at row #%d\n", getpid(), row);
+    exit(EXIT_SUCCESS);
+
+
 
 }
 
@@ -142,43 +175,19 @@ int main(int argc, char **argv)
     pid_t* child_pids = NULL;
     int child_count = 0;
 
+    // the issue with solving it this way is that for second and third row, this problem
+    // will solve for the second row with an empty first row
+
+    // What are the safe guards I can put in place?
+
+    // Easier way: recursion until solution reached, or no solution reached
+
+    int solutions = 0;
+    SolutionFinder(grid, 0, m, n, solutions, pipefd);
 
 
-    for (int i = 0; i < m; i++) {
-    	for (int j = 0; j < n; j++) {
-    		if (checkQueenPlacement(grid, i, j, m, n)) {
-                pid_t child_pid = fork();
-                if (child_pid == -1) {
-                    perror("fork");
-                    exit(EXIT_FAILURE);
-                } else if (child_pid == 0) {
-                    *(*(grid + i) + j) = 'Q';
-                    // Child process logic
-                    // ...
-                    exit(EXIT_SUCCESS);
-                } else {
-                    // Parent process
-                    child_count++;
-                    child_pids = (pid_t*)realloc(child_pids, child_count * sizeof(pid_t));
-                    child_pids[child_count - 1] = child_pid;
-                }
-            }
-    	}
-    }
 
-    // Parent process waits for all child processes to complete
-    for (int i = 0; i < child_count; i++) {
-        int status;
-        pid_t child_pid = waitpid(child_pids[i], &status, 0);
-        if (child_pid == -1) {
-            perror("waitpid");
-            exit(EXIT_FAILURE);
-        }
-        if (WIFEXITED(status)) {
-            int exit_status = WEXITSTATUS(status);
-            // Process the exit status of the child if needed
-        }
-    }
+    
 
 
 
@@ -190,5 +199,5 @@ int main(int argc, char **argv)
         free(*(grid+i));
     }
     free(grid);
-    free(pipfd);
+    free(pipefd);
 }
